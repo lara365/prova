cd3fdf1eb01c39badce66e520b7b3f05ed3ede0b
lara <larajhovanasantosdeoliveirabue@gmail.com>
2025-06-11 11:55:06 -0300

    fiz coisa

---
+++ reserva/backend/backend/logicaProgramacao/03_lacos.py
@@ -0,0 +1,18 @@
+# for 
+for i in range(5):
+  print("Contando:" , i)
+
+
+# while 
+senha = ""
+while senha != "Lara":
+      senha = input("digite a senha: ")
+      print("Senha incorreta")
+
+print ("Acesso liberado. ")
+
+
+#atividade numeros de 1 a 10 
+
+for i in range (1,10):
+    print( "somando", i)
---
+++ reserva/backend/backend/logicaProgramacao/04_def.py
@@ -0,0 +1,18 @@
+def ola(nome):
+    return "Olá, " + nome
+
+
+print(ola("Lara"))
+
+def media(t1,t2,t3):
+    return(t1 + t2 + t3)/3
+
+print (media(80,70,70))
+
+def par(numero):
+    if (numero % 2 == 0):
+        return print( "é par")
+    else:
+            return print ("é impar")
+
+print(9)
