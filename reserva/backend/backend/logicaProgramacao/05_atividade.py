e0d7ba9bbc538720d3875a5a5a9a52788a186b3e
lara <lara31@gmail.com>
2025-06-25 11:39:34 -0300

    Fiz atividades

---
+++ reserva/backend/backend/logicaProgramacao/05_atividade.py
@@ -0,0 +1,15 @@
+#def contar_ate_dez():
+ #   for i in range (1,11):
+ #    print (i)
+#contar_ate_dez () 
+
+#nome  = input("digite um nome: ")  
+#quantidade = int(input("digite a quantidade do numero desejado: "))
+#for i in range (quantidade):
+  #  print (nome)
+
+soma = 0
+for i in range (5):
+        numero = float(input(f"Digite o numero {i+1}: "))
+        soma += numero
+        print(f"A soma dos 5 numeros é: {soma} ")
