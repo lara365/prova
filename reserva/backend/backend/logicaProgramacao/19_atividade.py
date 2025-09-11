0342c12bc7f460250214c118d38957a5d9571744
lara <larajhovanasantosdeoliveirabue@gmail.com>
2025-09-11 09:59:41 -0300

    atividade avaliativa

--- reserva/backend/backend/logicaProgramacao/19_atividade.py
+++ reserva/backend/backend/logicaProgramacao/19_atividade.py
@@ -5,35 +5,60 @@ categoria = []
 id_categoria = 0
 id_produto = 0
 
+def carregar_arquivo(eletronico):
+    try:
+        with open(eletronico, 'r') as arquivo:
+            dados = json.load(arquivo)
+            print(f"Arquivo {eletronico} carregado!")
+            return dados
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado. Criando novo arquivo.")
+        return []
+
+
 def exibir_menu():
     print(".........loja eletronico.........")
     print("1.Cadastrar Categoria")
-    print("2.listar categoria")
-    print("3.categoria associada")
+    print("2.cadastrar produto")
+    print("3.listar produto")
     print("4.sair")
 
 def cadastrar_categoria():
-     print("/cadastrar produto:")
-     nome_categoria = input ("Nome: ")
-     id_categoria +=1
+    global id_categoria
+    print("/cadastrar produto:")
+    nome_categoria = input ("Nome: ")
+    id_categoria +=1
 
-     armazem_categoria = {
-          "nome_categoria" : nome_categoria,
-          "id_categoria" : id_categoria
+    armazem_categoria = {
+        "nome_categoria" : nome_categoria,
+        "id_categoria" : id_categoria
      }
+
+    categoria.append(armazem_categoria)
+    print("salvo")
      
 def cadastrar_produto():
-     id_produto +=1
-     nome_produto = input ("nome:")
-     preco = input ("preco:")
-     id_categoria_associada = input
+    global id_produto
+    id_produto +=1
+    nome_produto = input ("nome:")
+    preco = input ("preco:")
+    id_categoria_associada = input
 
-     armazem_produto = {
-     "nome_produto" : nome_produto,
-          "id_produto" : id_produto,
-          "preco ": preco ,
-          "id_categoria_associada " : id_categoria_associada,}
+    armazem_produto = {
+        "nome_produto" : nome_produto,
+        "id_produto" : id_produto,
+        "preco ": preco ,
+        "id_categoria_associada " : id_categoria_associada,}
      
+def listar_produtos():
+    if not produto:
+        print("Nenhum produto cadastrado.")
+        return
+
+    print("\n--- PRODUTOS CADASTRADOS ---")
+    
+
+
 while True:
     exibir_menu()
     opcao = input("escolha uma opção: ")
@@ -43,14 +68,9 @@ while True:
     elif opcao == '2':
         cadastrar_produto()
     elif opcao == '3':
-            listar_produtos()
+        listar_produtos()
     elif opcao == '4':
             print("Encerrando sistema. Até logo!")
             break
     else:
             print("Opção inválida. Tente novamente.")
-
-
-
-    
-     
